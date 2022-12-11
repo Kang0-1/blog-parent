48a73d53aa93d933eaf2a56662597d2f8075d70f
kang <1215285086@qq.com>
2022-12-12 00:49:33 +0800

    12.12 登录功能部分接口实现2

--- blog-api/src/main/java/com/kang/blog/service/impl/SysUserServiceImpl.java
+++ blog-api/src/main/java/com/kang/blog/service/impl/SysUserServiceImpl.java
@@ -3,12 +3,15 @@ package com.kang.blog.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.kang.blog.entity.SysUser;
 import com.kang.blog.mapper.SysUserMapper;
+import com.kang.blog.service.LoginService;
 import com.kang.blog.service.SysUserService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.kang.blog.vo.ErrorCode;
 import com.kang.blog.vo.Result;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -20,6 +23,10 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class SysUserServiceImpl extends ServiceImpl<SysUserMapper, SysUser> implements SysUserService {
+
+    @Resource
+    private LoginService loginService;
+
 
     @Override
     public SysUser findUserById(Long authorId) {
